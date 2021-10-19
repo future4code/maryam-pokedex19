873d58855e8f8eecb896b645ddc63c4025aa6711
Matheus Guilherme Schade <matheus_schade@hotmail.com>
2021-10-19 19:22:52 -0300

    renderizada lista de cards com pokemons e criada página com detalhes por ID

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,5 +1,5 @@
 export const HeaderColor = "#ff9c18";
 export const BackGroundColor = "#ffeecd";
-export const CardColor = "#832900";
+export const CardColor = "#eede7b";
 
 // for other colors, access https://pokepalettes.com/
--- src/screens/PokemonDetail/PokemonDetail.js
+++ src/screens/PokemonDetail/PokemonDetail.js
@@ -1,11 +1,44 @@
 import Header from "../../components/Header/Header"
+import { useParams } from "react-router-dom"
+import { urlBase } from "../../constants/url.js"
+import { useState, useEffect } from "react"
+import axios from "axios"
 
-const PokemonDetail = () => {
+const PokemonDetail = (id) => {
+    const params = useParams()
+    const [chosenPoke, setChosenPoke] = useState([])
+
+    useEffect(() => {
+        getPokeDetail()
+    }, [])
+
+    const getPokeDetail = (id) => {
+        axios.get(`${urlBase}/${params.id}`)
+            .then((response) => {
+                setChosenPoke(response.data)
+                console.log(response.data)
+            }).catch((error) => {
+                alert(`Erro: `, error)
+            })
+    }
+
+    console.log(`Utilizado Path Params: `, params)
 
     return (
         <div>
             <Header />
-            PokemonDetail</div>
+
+            {chosenPoke ? (
+                <div>
+                    <h1>{chosenPoke.name}</h1>
+
+                </div>
+
+            ) : (
+                <h2>Carregando...</h2>
+            )}
+
+        </div>
     )
 }
 
