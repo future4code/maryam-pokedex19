f3c71eeda96bbbfb0bd7bf10e30eda9183da8cc3
LuisH3n <85587362+LuisH3n@users.noreply.github.com>
2021-10-21 17:04:53 -0300

    Adcionando cards a pokedex

--- src/screens/Pokedex/Pokedex.js
+++ src/screens/Pokedex/Pokedex.js
@@ -2,24 +2,32 @@ import Header from "../../components/Header/Header.js"
 import { GlobalContext } from "../../global/contexts/GlobalContext";
 import { Card, ContainerButtons } from "./styled"
 import { useContext } from "react"
+import { GridCard } from "../PokemonList/styled.js";
 
 const Pokedex = () => {
     const { pokedex, setPokedex } = useContext(GlobalContext);
 
-    console.log(pokedex)
+    pokedex.forEach((item) =>console.log(item))
+
     return (
         <div>
 
             <Header />
-            {/* {pokedex.length > 0 && pokedex.map((item) => <Card key={item.id}>
+
+            <GridCard>
+            {pokedex.length > 0 && pokedex.map((item) => <Card key={item.id}>
                 <img src={item.sprites.versions['generation-v']['black-white'].animated.front_default} />
                 <h2>{(item.name).capitalize()} </h2>
                 <ContainerButtons>
-                    <button >Adicionar à Pokedex</button>
+                    <button >Remover da Pokedex</button>
                     <button >Ver detalhes</button>
                 </ContainerButtons>
             </Card>
-            )} */}
+            )}
+            </GridCard>
+            
+            
+
 
         </div>
     )
